4d3137055a88302f756c0ad380a536266976cea0
Pedro Garcia holguin <pgarcia-@c3r12s5.42madrid.com>
2023-01-03 18:12:47 +0100

    post free

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: pgarcia- <pgarcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:21:31 by pgarcia-          #+#    #+#             */
-/*   Updated: 2022/10/26 15:44:06 by pgarcia-         ###   ########.fr       */
+/*   Updated: 2022/11/02 18:01:50 by pgarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	static *char buff;  /*Almacenamos aquí todo lo leido hasta el momento*/
+	size_t size;		/*Numero de bytes que leeremos*/
+	size_t = cnt;		/*Contador donde guardaremos el resultado de read
+						ya que read devuelve el numero de caracteres
+						REALMENTE leidos, ya que nosotros podemos 
+						indicarle que lea 100 pero si hay 30 solo leera esos*/
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return(NULL);
+	buff = (char *)malloc(sizeof(char));
+	if (buff == 0)
+		return(0);
+		
+	cnt = read(fd, buff, size);
+
+		if (buff != 0)
+		{
+			
+		}
+	return (buff);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: pgarcia- <pgarcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:21:35 by pgarcia-          #+#    #+#             */
-/*   Updated: 2022/10/28 12:25:19 by pgarcia-         ###   ########.fr       */
+/*   Updated: 2022/11/02 10:33:58 by pgarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GET_NEXT_LINE
 # define FT_GET_NEXT_LINE
+#ifndef	BUFFER_SIZE
+# define BUFFER_SIZE 42
+
 
 #include <stdio.h>
 #include <stdlib.h>
